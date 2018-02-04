779d1a561b6d5c96b9bde29e10ead8d4f363f957
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-02-03 19:48:49 -0600

    add my tweets and spotify

--- liri.js
+++ liri.js
@@ -1,17 +1,50 @@
 require("dotenv").config();
 
 const keys = require("./keys.js");
-var Spotify = require('node-spotify-api');
-var Twitter = require('twitter');
-var request = require("request")
+const Spotify = require('node-spotify-api');
+const Twitter = require('twitter');
+const request = require("request")
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+const spotify = new Spotify(keys.spotify);
+const client = new Twitter(keys.twitter);
 
-var commmands = process.argv[2];
-var item = process.argv[3];
+let commmands = process.argv[2];
 
+let params = {
+    count: 20,
+    screen_name: "QuanGaoG"
+};
 //commands//my-tweets//spotify-this-song//movie-this//do-what-it-says
 
-
-
+switch (commmands) {
+    case "my-tweets":
+        console.log("Here're the 20 latest tweets")
+        client.get("statuses/user_timeline", params)
+            .then(function (tweets) {
+                for (let i = 0; i < tweets.length; i++) {
+                    let el = tweets[i];
+                    console.log(`\nAt ${el.created_at}, ${el.user.name} tweeted "${el.text}"`);
+                }
+            })
+            .catch(function (error) {
+                throw error;
+            })
+        break;
+    case "spotify-this-song":
+        let song = process.argv[3] || "The Sign Ace of Base"
+        spotify.search({
+                type: "track",
+                query: song
+            })
+            .then(function (response) {
+                let hit = response.tracks.items[0];
+                console.log(`\nArtist(s): ${hit.artists[0].name};
+                    \nSong name: ${hit.name};
+                    \nPreview: ${hit.preview_url};
+                    \nAlbum: ${hit.album.name}\n`
+                )
+            })
+            .catch(err => console.log(err))
+    // case ""
+        
+}
